cd709bf03010d6da08718c316868c7716cfdef29
MmdFucker <104726242+MmdFucker@users.noreply.github.com>
2022-05-01 19:30:20 +0430

    Create Hack-Rubika.py

---
+++ Hack-Rubika.py
@@ -0,0 +1,28 @@
+ 
+ ​from​ ​time​ ​import​ ​sleep 
+  
+  
+ ​print​(​"###################################"​) 
+ ​print​(​"|                                 |"​) 
+ ​print​(​"|                                 |"​) 
+ ​print​(​"|    h a c k     r u b i k a      |"​) 
+ ​print​(​"|                                 |"​) 
+ ​print​(​"|                                 |"​) 
+ ​print​(​"|---------------------------------|"​) 
+ ​print​(​"|                                 |"​) 
+ ​print​(​"|   Manufacturer :   MMDFAKER     |"​) 
+ ​print​(​"|                                 |"​) 
+ ​print​(​"|                                 |"​) 
+ ​print​(​"###################################"​) 
+  
+  
+ ​hack​ ​=​ ​input​(​"TARGET  PHONE or ID : "​) 
+  
+ ​l​ ​=​ [​"hacking the rubika 18%..."​, ​"hacking the rubika 32%...."​, ​"hacking the rubika 58%....."​, ​"hacking the rubika 76%......"​, ​"hacking the rubika 99%......."​, ​" * hacking failed - Done! Crack the code  rubika target :* "​] 
+  
+ ​for​ ​i​ ​in​ ​l​: 
+ ​    ​print​(​"​\r​"​ ​+​ ​i​, ​end​=​""​) 
+ ​    ​sleep​(​1.5​) 
+ ​print​() 
+  
+ ​print​(​"code : 642863"​ ​or​ ​"947643"​ ​or​ ​"023272"​)
